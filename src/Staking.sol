df11a84bf7f87aef3e3265d3096e61f557268a4f
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-20 16:00:41 +0530

    Update Staking.sol

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
-import "src/AshCoin.sol";
+import { AshCoin } from "../src/AshCoin.sol";
 
 contract Staking {
     mapping(address => uint) public balances;
     mapping(address => uint) public unClaimedRewards;
-    mapping(address => uint) public lastUpdated;
+    mapping(address => uint) public lastUpdateTime;
 
     constructor() {}
 
@@ -14,21 +14,45 @@ contract Staking {
 
     function stake() public  payable {
         require(msg.value > 0);
+
+        if(lastUpdateTime[msg.sender] == 0) {
+            lastUpdateTime[msg.sender] = block.timestamp;
+        }
+        else{
+            unClaimedRewards[msg.sender] = (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+            lastUpdateTime[msg.sender] = block.timestamp;
+        }
         balances[msg.sender] += msg.value;
     }
 
     function unStake(uint _amount) public {
         require(balances[msg.sender] >= _amount);
+
+        unClaimedRewards[msg.sender] = (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+        lastUpdateTime[msg.sender] = block.timestamp;
+
         payable(msg.sender).transfer(_amount);
         balances[msg.sender] -= _amount;
     }
 
-    function getRewards() public {
+    function getRewards(address _addr) public view returns(uint) {
+        uint currentReward = unClaimedRewards[_addr];
+        uint updateTime = lastUpdateTime[_addr];
+        uint newReward = (block.timestamp - updateTime) * balances[_addr];
+        return (currentReward + newReward);
+
 
     }
 
     function claimRewards() public {
+        //uint currentReward = unClaimedRewards[msg.sender];
+        //uint updateTime = lastUpdateTime[msg.sender];
+       // uint newReward = (block.timestamp - updateTime) * balances[msg.sender];
 
+        // For Miniting Tokens
+
+        unClaimedRewards[msg.sender] = 0;
+        lastUpdateTime[msg.sender] = block.timestamp;
     }
     
     function balanceOf(address _address) public view returns (uint) {
